700452daab83221b3cd7ee404012a3776a85b58f
DemisRusso <demisjunior@hotmail.com>
2019-12-14 19:25:50 -0300

    challenge 1 and 2

---
+++ challenge01&02.js
@@ -0,0 +1,40 @@
+//Challenge -01 and 02
+function soma(a, b) {
+    return a + b;
+}
+
+var somar = soma(10, 10) + 5;
+//console.log(somar)
+
+var svalor;
+
+function add() {
+    a = 2;
+    return 'valor: ' + a;
+}
+
+svalor = add();
+
+function arg(a, b, c) {
+    if (a != undefined && b === undefined && c === undefined) {
+        return a;
+    }
+
+    else if (a != undefined && b != undefined && c === undefined) {
+        return a + b;
+    }
+
+    else if (a !== undefined && b !== undefined && c !== undefined) {
+        return (a + b) / c;
+    }
+
+    else if (a == undefined && b == undefined && c == undefined) {
+        return false;
+    } else {
+        return null;
+    }
+}
+
+console.log(arg(1, 2, 3));
+
+// ==================================== Exercises ================================
