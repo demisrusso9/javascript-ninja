df176fdab98f73f6ebad1796cffaf164638abf8c
DemisRusso <demisjunior@hotmail.com>
2020-01-28 19:43:06 -0200

    objects

--- challenge03.js
+++ challenge03.js
@@ -33,7 +33,6 @@ var pessoa = {
         var sexo = pessoa.sexo === 'Feminino' ? 'a ' : 'o';
         var idade = pessoa.idade === 1 ? 'ano' : 'anos';
         var caminhou = pessoa.caminhouMetros === 1 ? 'metro' : 'metros';
-        
         return 'Olá, eu sou ' + sexo + pessoa.nome + ' ' + pessoa.sobrenome + ', tenho ' + pessoa.idade + ' ' + idade + ', ' + pessoa.altura +
             ', meu peso é ' + pessoa.peso + ' e, só hoje, eu já caminhei ' + pessoa.caminhouMetros + ' ' + caminhou + '!';
     }
