cb13e745c3689f412e8a6d7a62c6ec450f92f094
DemisRusso <demisjunior@hotmail.com>
2020-02-03 12:45:11 -0200

    object I [adding template literals]

--- challenge03.js
+++ challenge03.js
@@ -22,17 +22,22 @@ var pessoa = {
     },
 
     nomeCompleto() {
-        return 'Olá, meu nome é ' + pessoa.nome + ' ' + pessoa.sobrenome;
+        return 'Olá, meu nome é' + pessoa.nome + pessoa.sobrenome;
     },
 
     mostrarIdade() {
-        return 'Eu tenho ' + pessoa.idade + ' anos';
+        return 'Eu tenho' + pessoa.idade + 'anos';
     },
 
     apresentacao() {
-        var sexo = pessoa.sexo === 'Feminino' ? 'a ' : 'o';
-        var idade = pessoa.idade === 1 ? 'ano' : 'anos';
+        var sexo = pessoa.sexo === 'Feminino' ? 'a' : 'o';
+        var pluralAno = pessoa.idade === 1 ? 'ano' : 'anos';
         var caminhou = pessoa.caminhouMetros === 1 ? 'metro' : 'metros';
+
+        var { nome, sobrenome, idade, peso, altura, caminhouMetros } = pessoa;
+
+        return `Olá, eu sou ${sexo} ${nome} ${sobrenome}, tenho ${idade} ${pluralAno}, ${altura} de altura, meu peso é ${peso} kg e, só hoje, eu já caminhei ${caminhouMetros} ${caminhou}!`;
+
         return 'Olá, eu sou ' + sexo + pessoa.nome + ' ' + pessoa.sobrenome + ', tenho ' + pessoa.idade + ' ' + idade + ', ' + pessoa.altura +
             ', meu peso é ' + pessoa.peso + ' e, só hoje, eu já caminhei ' + pessoa.caminhouMetros + ' ' + caminhou + '!';
     }
